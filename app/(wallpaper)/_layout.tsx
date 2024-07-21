622080e687d35d16b6a165d54240dd7db9341683
ayushaggarwal600 <ayushaggarwal600@gmail.com>
2024-07-21 16:35:13 +0530

    Commit

---
+++ app/(wallpaper)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+
+export default function HomeLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="wallpaper" 
+        options={
+            {
+                headerShown: false
+            }
+        }
+      />
+    </Stack>
+  );
+}
